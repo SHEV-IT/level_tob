9f3a48a939f91a614f70ae3e9d704b3ab6b495ed
Oleg Gromyak <oagromyak@gmail.com>
2017-05-04 13:56:00 +0300

    Fix module caching

--- projects/__init__.py
+++ projects/__init__.py
@@ -3,6 +3,7 @@ import os
 import imp
 import inspect
 import sys
+import types
 import constants as c
 
 modules = dict()
@@ -18,7 +19,17 @@ def get_subclass(mod, cls):
 for project_id in c.VK_PROJECTS:
     project_name = c.VK_PROJECTS[project_id]['name']
     project_module = c.VK_PROJECTS[project_id].get('module', project_name)
-    sys.modules.pop(project_module, None)  # prevent loading cached module
+
+    module_name = 'projects.%s' % project_module
+    # get a reference to each loaded module
+    loaded_package_modules = dict([
+                                      (key, value) for key, value in sys.modules.items()
+                                      if key.startswith(module_name) and isinstance(value, types.ModuleType)])
+
+    # delete references to these loaded modules from sys.modules
+    for key in loaded_package_modules:
+        del sys.modules[key]
+
     module = __import__(project_module, globals(), locals(), [], -1)
     bot_class = get_subclass(module, Bot)
     if bot_class:
